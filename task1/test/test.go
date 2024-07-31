957b0accbd95fa6239ed82f2cf687933b7beb5ad
me-ron <meronkiflayyy@gmail.com>
2024-07-31 15:12:00 +0300

    task3 done

--- task1/test/test.go
+++ task1/test/test.go
@@ -1,12 +1,19 @@
-package main 
+package main
 
 import (
+	"bufio"
 	sys "fmt"
+	"os"
 	grade "task1/grd"
 )
 
 func test(){
 	var name string; var num float64
+	reader := bufio.NewReader(os.Stdin)
+
+	line, _ := reader.ReadString('\n')
+
+	sys.Println(line)
 	sys.Println("Enter your name:")
 	sys.Scan(&name)
 	sys.Println("Enter the number of courses you take:")
