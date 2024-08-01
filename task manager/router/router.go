8a037a360a58ccda810281b3852c21265be00b0c
me-ron <meronkiflayyy@gmail.com>
2024-08-01 17:12:21 +0300

    task4 router

--- task manager/router/router.go
+++ task manager/router/router.go
@@ -1,17 +1,23 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"task_manager/controllers"
+	"task_manager/models"
+
+	"github.com/gin-gonic/gin"
 )
+
+var tasks []*models.Task
 
 func Run(){
 	router := gin.Default()
 
-	router.GET("api/tasks", controllers.GetAllTasks)
-	router.GET("api/tasks/:id")
-	router.POST("api/tasks")
-	router.PUT("api/tasks/:id")
-	router.DELETE("api/tasks/:id")
+	router.GET("api/tasks", controllers.GetAllTasks(tasks))
+	router.GET("api/tasks/:id", controllers.GetTaskById(tasks))
+	router.POST("api/tasks", controllers.PostTask(tasks))
+	router.PUT("api/tasks/:id", controllers.UpdateTask(tasks))
+	router.DELETE("api/tasks/:id", controllers.DeleteTask(tasks))
+
+	router.Run("localhost:8080/")
 
 }
