0f9005df09b475d844b20c55beafe750f5e503fc
me-ron <meronkiflayyy@gmail.com>
2024-08-01 17:03:45 +0300

    update controllers

--- task manager/controllers/task_controllers.go
+++ task manager/controllers/task_controllers.go
@@ -7,59 +7,73 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetAllTasks(c *gin.Context, tasks []*models.Task){
-	c.IndentedJSON(http.StatusOK, tasks)
+func GetAllTasks(tasks []*models.Task) gin.HandlerFunc{
+		return func (c *gin.Context){
+			c.IndentedJSON(http.StatusOK, tasks)
 
-}
-
-func GetTaskById(c *gin.Context, tasks []*models.Task){
-	id := c.Param("id")
-	for i := range len(tasks){
-		if tasks[i].ID == id {
-			task := *tasks[i]
-			c.IndentedJSON(http.StatusOK, task)
-			return
 		}
-	}
-
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Taks not found"})
-
 }
 
-func PostTask(c *gin.Context, tasks []*models.Task){
-	var task models.Task
+func GetTaskById(tasks []*models.Task) gin.HandlerFunc{
 
-	if err := c.BindJSON(&task); err != nil{
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "invalid request"})
-	}
-
-	tasks = append(tasks, &task)
-	c.IndentedJSON(http.StatusCreated, *tasks[len(tasks) - 1])
-}
-
-func DeleteTask(c *gin.Context, tasks []*models.Task){
-	id := c.Param("id")
-	for i := range len(tasks){
-		if tasks[i].ID == id{
-			tasks = append(tasks[:i], tasks[i + 1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"messages" : "deleted successfully"})
-			return
+	return func (c *gin.Context){
+		id := c.Param("id")
+		for i := range len(tasks){
+			if tasks[i].ID == id {
+				task := *tasks[i]
+				c.IndentedJSON(http.StatusOK, task)
+				return
+			}
 		}
+
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Taks not found"})
+
 	}
-	c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "no task of this id"})
 }
 
-func UpdateTask(c *gin.Context, tasks []*models.Task){
-	var task models.Task
+func PostTask(tasks []*models.Task) gin.HandlerFunc{
+	return func (c *gin.Context){
+		var task models.Task
 
-	if err := c.BindJSON(&task); err != nil{
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "invalid request"})
-	} 
+		if err := c.BindJSON(&task); err != nil{
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "invalid request"})
+		}
 
-	for i := range len(tasks){
-		if tasks[i].ID == task.ID{
-			tasks[i] = &task
-			c.IndentedJSON(http.StatusOK, task)
+		tasks = append(tasks, &task)
+		c.IndentedJSON(http.StatusCreated, *tasks[len(tasks) - 1])
+	}
+
+}
+
+
+func DeleteTask(tasks []*models.Task) gin.HandlerFunc{
+	return func (c *gin.Context){
+		id := c.Param("id")
+		for i := range len(tasks){
+			if tasks[i].ID == id{
+				tasks = append(tasks[:i], tasks[i + 1:]...)
+				c.IndentedJSON(http.StatusOK, gin.H{"messages" : "deleted successfully"})
+				return
+			}
+		}
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "no task of this id"})
+	}
+
+}
+
+func UpdateTask(tasks []*models.Task) gin.HandlerFunc{
+	return func (c *gin.Context){
+		var task models.Task
+
+		if err := c.BindJSON(&task); err != nil{
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "invalid request"})
+		} 
+
+		for i := range len(tasks){
+			if tasks[i].ID == task.ID{
+				tasks[i] = &task
+				c.IndentedJSON(http.StatusOK, task)
+			}
 		}
 	}
 }
--- task manager/router/router.go
+++ task manager/router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"task_manager/controllers"
+)
+
+func Run(){
+	router := gin.Default()
+
+	router.GET("api/tasks", controllers.GetAllTasks)
+	router.GET("api/tasks/:id")
+	router.POST("api/tasks")
+	router.PUT("api/tasks/:id")
+	router.DELETE("api/tasks/:id")
+
+}
