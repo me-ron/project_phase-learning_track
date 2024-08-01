f1a00fc56d1c85296805899e52b6c9a950e8943b
me-ron <meronkiflayyy@gmail.com>
2024-08-01 15:14:20 +0300

    task4 model

--- task manager/models/task.go
+++ task manager/models/task.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	DueDate     time.Time `json:"due_date"`
+   }
