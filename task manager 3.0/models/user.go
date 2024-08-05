0084a985bf5bf99b5f59fe1c6089a6c8e29bf146
me-ron <meronkiflayyy@gmail.com>
2024-08-05 15:45:00 +0300

    created the user model

---
+++ task manager 3.0/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password  string			 `json:"password" bson:"password"`
+	IsAdmin   bool 				 `json:"isadmin" bson:"isadmin"`
+}
