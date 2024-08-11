56e62bb6cb90eb33c24b9553a56328b5eccb3490
mer-on <meronkiflayyy@gmail.com>
2024-08-11 18:26:43 +0300

    worked on 3 of the infrastructures

--- clean task_manager/infrastructure/auth_middleware.go
+++ clean task_manager/infrastructure/auth_middleware.go
@@ -1,1 +1,33 @@
 package infrastructure
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var secretKey = []byte(os.Getenv("JWT_SECRET"))
+
+func AuthMiddleware(c *gin.Context) {
+
+	// check for the authorization
+	auth := c.GetHeader("Authorization")
+
+	if auth == "" {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"msg": "NOT AUTHORIZED"})
+		c.Abort()
+		return
+	}
+
+	err := TokenValidate(auth)
+
+	if err != nil {
+		c.JSON(401, gin.H{"error": err.Error()})
+		c.Abort()
+		return
+	}
+
+	// move to the next step
+	c.Next()
+}
--- clean task_manager/infrastructure/jwt_service.go
+++ clean task_manager/infrastructure/jwt_service.go
@@ -1,1 +1,31 @@
 package infrastructure
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func TokenValidate(auth string) error {
+	authSplit := strings.Split(auth, " ")
+
+	if len(authSplit) != 2 || strings.ToLower(authSplit[0]) != "bearer" {
+		return errors.New("not authorized")
+	}
+
+	token, err := jwt.Parse(authSplit[1], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil || !token.Valid {
+		return errors.New("invalid jwt")
+	}
+
+	return nil
+}
