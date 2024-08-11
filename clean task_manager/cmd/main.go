0d39337a1d8fa36b1d1e268e2943c5dd569bee3e
mer-on <meronkiflayyy@gmail.com>
2024-08-11 22:07:54 +0300

    finalized main.go

--- clean task_manager/cmd/main.go
+++ clean task_manager/cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"task_manager/database"
+	"task_manager/delivery/routes"
+)
+
+func main(){
+	fmt.Println("Server started")
+	client, err := database.DB()
+	if err != nil{
+		log.Fatal(err.Error())
+		return
+	}
+	defer func(){
+		err := client.Disconnect(context.TODO())
+		if err != nil{
+			log.Fatal(err.Error())
+		}
+		}()
+
+	db := client.Database("task_manager")
+	routes.Run(db)
+
+}
