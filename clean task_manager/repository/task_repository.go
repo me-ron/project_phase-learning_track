c488f55d61be26b9174712f96c59eda22a8fb07f
mer-on <meronkiflayyy@gmail.com>
2024-08-11 17:41:16 +0300

    setup task repo and task usecase

--- clean task_manager/repository/task_repository.go
+++ clean task_manager/repository/task_repository.go
@@ -1,1 +1,40 @@
 package repository
+
+import (
+	"task_manager/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type TaskRepo struct {
+	coll mongo.Collection
+}
+
+func NewTaskRepository(db mongo.Database, name string) *TaskRepo{
+	return &TaskRepo{
+		coll: *db.Collection(name),
+	}
+}
+
+func (TR *TaskRepo) CreateTask(task domain.Task) (domain.Task, error){
+
+}
+
+func (TR *TaskRepo) DeleteTaskById(id string, userId primitive.ObjectID) error{
+
+}
+
+func (TR *TaskRepo) UpdateTaskById(id string, task domain.Task) (domain.Task, error){
+
+}
+
+func (TR *TaskRepo) GetAllTasks(filter bson.M) ([]domain.Task, error){
+
+}
+
+func (TR *TaskRepo) FindTaskById(id string, userId primitive.ObjectID) (domain.Task, error){
+
+}
+
--- clean task_manager/useCase/task_usecase.go
+++ clean task_manager/useCase/task_usecase.go
@@ -1,1 +1,38 @@
 package useCase
+
+import (
+	"task_manager/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TaskUC struct {
+	repo domain.TaskRepository
+}
+
+func NewTaskUC(repository domain.TaskRepository) *TaskUC{
+	return &TaskUC{
+		repo: repository,
+	}
+}
+
+func (TUC *TaskUC) PostTask(task domain.Task, user domain.DBUser) (domain.Task, error) {
+
+}
+
+func (TUC *TaskUC) GetTasks(filter bson.M) ([]domain.Task, error){
+
+}
+
+func (TUC *TaskUC) GetTask(id string, userId primitive.ObjectID) (domain.Task, error){
+
+}
+
+func (TUC *TaskUC) UpdateTask(id string, task domain.Task, user domain.DBUser) (domain.Task, error){
+
+}
+
+func (TUC *TaskUC) DeleteTask(id string, userId primitive.ObjectID) error{
+	
+}
