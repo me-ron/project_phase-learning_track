ed13ba390d9399259e362bb11814d86e2c6880f7
mer-on <meronkiflayyy@gmail.com>
2024-08-11 18:51:06 +0300

    set up user usecases and repositories

--- clean task_manager/repository/user_repository.go
+++ clean task_manager/repository/user_repository.go
@@ -1,1 +1,45 @@
 package repository
+
+import (
+	"context"
+	"task_manager/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type UserRepo struct {
+	coll *mongo.Collection
+}
+
+func (UR *UserRepo) EnsureIndexes() error {
+	indexModel := mongo.IndexModel{
+		Keys:    bson.M{"email": 1}, 
+		Options: options.Index().SetUnique(true),
+	}
+	
+	_, err := UR.coll.Indexes().CreateOne(context.TODO(), indexModel)
+	return err
+}
+
+func NewUserRepo(db mongo.Database, name string) (*UserRepo, error) {
+	UR := &UserRepo{
+		coll : db.Collection(name),
+	}
+
+	// Ensure indexes are created
+	if err := UR.EnsureIndexes(); err != nil {
+		return nil, err
+	}
+
+	return UR, nil
+}
+
+
+func (UR *UserRepo)FindByEmail(string) (domain.UserInput, error)
+func (UR *UserRepo)FindById(string) (domain.UserInput, error)
+func (UR *UserRepo)FindAllUsers() ([]domain.DBUser, error)
+func (UR *UserRepo)UpdateUserById(string, domain.UserInput, bool) (domain.DBUser, error)
+func (UR *UserRepo)CreateUser(domain.UserInput) (domain.DBUser, error)
+func (UR *UserRepo)DeleteUserByID(string) error
--- clean task_manager/useCase/user_usecase.go
+++ clean task_manager/useCase/user_usecase.go
@@ -1,1 +1,21 @@
 package useCase
+
+import "task_manager/domain"
+
+type UserUC struct {
+	repo domain.UserRepository
+}
+
+func NewUserUC(repository domain.UserRepository) *UserUC{
+	return &UserUC{
+		repo: repository,
+	}
+}
+
+func (UUC *UserUC)Login(domain.UserInput) (domain.DBUser, string, error)
+func (UUC *UserUC)Signup(domain.UserInput) (domain.DBUser, error)
+func (UUC *UserUC)GetUsers() ([]domain.DBUser, error)
+func (UUC *UserUC)GetUser(string) (domain.DBUser, error)
+func (UUC *UserUC)MakeAdmin(string) (domain.DBUser, error)
+func (UUC *UserUC)UpdateUser(string, domain.UserInput) (domain.DBUser, error)
+func (UUC *UserUC)DeleteUser(string) error
