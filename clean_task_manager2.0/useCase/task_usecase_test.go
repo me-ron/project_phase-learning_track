9fee3fae6caf17732f7e8ef24dbc9052b0544b06
mer-on <meronkiflayyy@gmail.com>
2024-08-14 08:30:09 +0300

    finished the usecase tests

--- clean_task_manager2.0/useCase/task_usecase_test.go
+++ clean_task_manager2.0/useCase/task_usecase_test.go
@@ -1,1 +1,92 @@
 package useCase
+
+import (
+	"task_manager/domain"
+	"task_manager/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TaskUsecaseSuite struct {
+	suite.Suite
+	mockRepo *mocks.TaskRepository
+	Task_UC *TaskUC
+}
+
+func (suite *TaskUsecaseSuite) SetupTest(){
+	suite.mockRepo = mocks.NewTaskRepository(suite.T())
+
+	suite.Task_UC = NewTaskUC(suite.mockRepo)
+}
+
+func (suite *TaskUsecaseSuite) TestPostTask(){
+	expetedTask := domain.Task{Title: "test title"}
+	suite.mockRepo.On("CreateTask", domain.Task{}).Return(expetedTask, nil)
+
+	task, err := suite.Task_UC.PostTask(domain.Task{}, domain.DBUser{})
+
+	suite.NoError(err)
+	suite.Equal(expetedTask, task)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestGetTasks() {
+	filter := bson.M{"userId": "some-user-id"}
+	expectedTasks := []domain.Task{{Title: "Task 1"}, {Title: "Task 2"}}
+
+	suite.mockRepo.On("GetAllTasks", filter).Return(expectedTasks, nil)
+
+	tasks, err := suite.Task_UC.GetTasks(filter)
+
+	suite.NoError(err)
+	suite.Equal(expectedTasks, tasks)
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestGetTask() {
+	id := "task-id"
+	userId := primitive.NewObjectID()
+	expectedTask := domain.Task{Title: "Task 1"}
+
+	suite.mockRepo.On("FindTaskById", id, userId).Return(expectedTask, nil)
+
+	task, err := suite.Task_UC.GetTask(id, userId)
+
+	suite.NoError(err)
+	suite.Equal(expectedTask, task)
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestUpdateTask() {
+	id := "task-id"
+	user := domain.DBUser{Email: "test@email.com"}
+	task := domain.Task{Title: "Updated Task"}
+
+	suite.mockRepo.On("UpdateTaskById", id, domain.Task{Title: "Updated Task", User: user}).Return(task, nil)
+
+	updatedTask, err := suite.Task_UC.UpdateTask(id, task, user)
+
+	suite.NoError(err)
+	suite.Equal(task, updatedTask)
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestDeleteTask() {
+	id := "task-id"
+	userId := primitive.NewObjectID()
+
+	suite.mockRepo.On("DeleteTaskById", id, userId).Return(nil)
+
+	err := suite.Task_UC.DeleteTask(id, userId)
+
+	suite.NoError(err)
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func TestTaskUseCaseSuite(t *testing.T){
+	suite.Run(t, new(TaskUsecaseSuite))
+}
--- clean_task_manager2.0/useCase/user_usecase_test.go
+++ clean_task_manager2.0/useCase/user_usecase_test.go
@@ -1,1 +1,132 @@
 package useCase
+
+import (
+	"task_manager/domain"
+	"task_manager/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type UserUsecaseSuite struct {
+	suite.Suite
+	mockRepo *mocks.UserRepository
+	mockPass_s *mocks.PasswordService
+	mockToken_s *mocks.TokenService
+	User_UC *UserUC
+}
+
+func (suite *UserUsecaseSuite) SetupTest(){
+	suite.mockRepo = mocks.NewUserRepository(suite.T())
+	suite.mockPass_s = mocks.NewPasswordService(suite.T())
+	suite.mockToken_s = mocks.NewTokenService(suite.T())
+
+	suite.User_UC = NewUserUC(suite.mockRepo, suite.mockPass_s, suite.mockToken_s)
+}
+
+func (suite *UserUsecaseSuite) TestLogin(){
+	suite.mockRepo.On("FindByEmail","test@email.com").Return(domain.UserInput{Email:"test@email.com"}, nil)
+	suite.mockPass_s.On("ComparePassword", "", "").Return(true, nil)
+	suite.mockToken_s.On("CreateToken", mock.MatchedBy(func(m interface{})bool{
+		_, ok := m.(domain.UserInput)
+		return ok
+	})).Return("NewToken", nil)
+
+	user, token, err := suite.User_UC.Login(domain.UserInput{Email: "test@email.com"})
+	suite.NoError(err)
+	suite.Equal("NewToken", token)
+	suite.Equal("test@email.com", user.Email)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+	suite.mockPass_s.AssertExpectations(suite.T())
+	suite.mockToken_s.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestSignup(){
+	suite.mockPass_s.On("HashPasword", "").Return("Hashed password", nil)
+	suite.mockRepo.On("CreateUser", domain.UserInput{Email:"test@email.com", 
+										Password: "Hashed password"}).Return(domain.DBUser{Email: "test@email.com"}, nil)
+	user, err := suite.User_UC.Signup(domain.UserInput{Email:"test@email.com"})
+	suite.NoError(err)
+	suite.Equal("test@email.com", user.Email)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+	suite.mockPass_s.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestGetUsers() {
+	expectedUsers := []domain.DBUser{
+		{Email: "user1@email.com"},
+		{Email: "user2@email.com"},
+	}
+	suite.mockRepo.On("FindAllUsers").Return(expectedUsers, nil)
+
+	users, err := suite.User_UC.GetUsers()
+
+	suite.NoError(err)
+	suite.Equal(expectedUsers, users)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestGetUser() {
+	expectedUser := domain.DBUser{Email: "test@email.com"}
+	suite.mockRepo.On("FindById", "some-id").Return(domain.UserInput{Email: "test@email.com"}, nil)
+
+	user, err := suite.User_UC.GetUser("some-id")
+
+	suite.NoError(err)
+	suite.Equal(expectedUser, user)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestMakeAdmin() {
+	expectedUser := domain.DBUser{Email: "test@email.com", IsAdmin: true}
+	suite.mockRepo.On("FindById", "some-id").Return(domain.UserInput{Email: "test@email.com"}, nil)
+	suite.mockRepo.On("UpdateUserById", "some-id", domain.UserInput{Email: "test@email.com"}, true).Return(expectedUser, nil)
+
+	user, err := suite.User_UC.MakeAdmin("some-id")
+
+	suite.NoError(err)
+	suite.Equal(expectedUser, user)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestUpdateUser() {
+	existingUser := domain.UserInput{Email: "test@email.com", Password: "oldpassword"}
+	updatedUser := domain.UserInput{Email: "updated@email.com", Password: "newpassword"}
+	hashedPassword := "hashedpassword"
+
+	suite.mockRepo.On("FindById", "some-id").Return(existingUser, nil)
+	suite.mockPass_s.On("HashPasword", "newpassword").Return(hashedPassword, nil)
+	suite.mockRepo.On("UpdateUserById", "some-id", mock.AnythingOfType("domain.UserInput"), false).
+		Return(domain.DBUser{Email: "updated@email.com"}, nil)
+
+
+	user, err := suite.User_UC.UpdateUser("some-id", updatedUser)
+
+
+	suite.NoError(err)
+	suite.Equal("updated@email.com", user.Email)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+	suite.mockPass_s.AssertExpectations(suite.T())
+}
+
+func (suite *UserUsecaseSuite) TestDeleteUser() {
+	suite.mockRepo.On("DeleteUserByID", "some-id").Return(nil)
+
+	err := suite.User_UC.DeleteUser("some-id")
+
+	suite.NoError(err)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+
+func TestUserUseCaseSuite(t *testing.T){
+	suite.Run(t, new(UserUsecaseSuite))
+}
